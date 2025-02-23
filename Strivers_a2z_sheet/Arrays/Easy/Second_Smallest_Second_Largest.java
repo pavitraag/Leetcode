f579dbde90a1c88c57024aef007f4a9d339112ac
Pavitraa G <100479594+pavitraag@users.noreply.github.com>
2025-02-23 12:32:53 +0530

    Create Second_Smallest_Second_Largest.java

---
+++ Strivers_a2z_sheet/Arrays/Easy/Second_Smallest_Second_Largest.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            String[] arr1Str = sc.nextLine().split(" ");
+            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
+            Solution ob = new Solution();
+            int ans = ob.getSecondLargest(arr);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int getSecondLargest(int[] arr) {
+         if(arr.length<2)
+         {return -1;}
+         
+         Arrays.sort(arr);
+         
+         int last = arr[arr.length-1];
+
+
+         for(int i=arr.length-2;i>=0;i--){
+             if(arr[i]!= last)
+               return arr[i];
+         }
+         return -1;
+        
+    }
+}
